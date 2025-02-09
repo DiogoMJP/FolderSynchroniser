d02397eeff88eeea360c37d96468cd085626b708
DiogoMJP <diogo.mj.pereira@gmail.com>
2025-02-09 23:49:19 +0000

    added File functionality to compare modification times

--- File.py
+++ File.py
@@ -7,6 +7,14 @@ class File():
         self.dest_list = []
 
     def update_path(self, path):
+        time = os.path.getmtime(path)
         if self.origin == None:
-            self.origin = {"path" : path, "time" : os.path.getmtime(path)}
+            self.origin = {"path" : path, "time" : time}
+        else:
+            if time > self.origin["time"]:
+                origin = self.origin
+                self.dest_list += [origin]
+                self.origin = {"path" : path, "time" : time}
+            else:
+                self.dest_list += [{"path" : path, "time" : time}]
         print(self.origin)
